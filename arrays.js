020c109029137e8c21c91e4061cc68469897d5b3
C Z <xzcynthiazheng@gmail.com>
2018-07-31 01:58:25 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+function addElementToBeginningOfArray(arr, element) {
+  var newArr = arr.unshift(element)
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element) {
+  arr.push(element)
+  return arr
+}
+
+
+function addElementToEndOfArray() {
+  
+}
+
+function destructivelyAddElementToEndOfArray() {
+  
+}
